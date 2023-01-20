6a8466cb004b6fd6c03b87472546ef9ba44319ae
danishfarooq83 <101505646+danishfarooq83@users.noreply.github.com>
2023-01-21 00:26:52 +0530

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,10 @@
+
+
+function btn(){
+    var name= document.getElementById("name").value;
+    var email= document.getElementById("email").value;
+   
+    alert("your name is  "+name);
+    alert("your email is  " +email);
+    
+}
